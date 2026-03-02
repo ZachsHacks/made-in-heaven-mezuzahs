fbc9f029b89b6ee545dad97388ac4ca83d5dfa70
Zach Weiss <coolshale@gmail.com>
2026-03-01 22:29:59 -0500

    Update mezuzah collection via admin panel

--- data/mezuzahs.js
+++ data/mezuzahs.js
@@ -11,6 +11,15 @@
 const MEZUZAHS = [
 
   {
+    images:     ["https://tvgx0maqcsilvttc.public.blob.vercel-storage.com/78432898-6599-4747-9C05-9196CF0DA44D_1_105_c-1.jpg"],
+    name:       "Dripping Aqua & Silver",
+    tagline:    "Raw Terracotta with Silver Leaf, dripping in Aqua",
+    price:      50,
+    categories: ["Medium (5-7″)","Raw Terracotta","With Silver Leaf"],
+    description: ``,
+  },
+
+  {
     images:     ["https://tvgx0maqcsilvttc.public.blob.vercel-storage.com/IMG_2392.jpeg"],
     name:       "Terra Gold",
     tagline:    "Terracotta with shin - Gold leaf",
