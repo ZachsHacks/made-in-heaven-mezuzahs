6e743bf8b5dbda6567bddd849f9f63dbb2b38f4a
Zach Weiss <coolshale@gmail.com>
2026-03-08 15:18:47 -0400

    Update mezuzah collection via admin panel

--- data/mezuzahs.js
+++ data/mezuzahs.js
@@ -165,11 +165,11 @@ const MEZUZAHS = [
 
   {
     images:     ["https://tvgx0maqcsilvttc.public.blob.vercel-storage.com/A6358F8C-7204-4F88-9473-9A12713627F1_1_102_o.jpeg"],
-    name:       "Golden Israel",
-    tagline:    "Pastel Holy Land in Gold",
-    price:      185,
-    categories: ["Medium (5-7″)","White Stone","With Gold Leaf"],
-    description: `Soft pastels depict the land of Israel on white stone, with gold leaf lettering adding sacred warmth and intention. Fired in the kiln, each detail connects your doorway to the Holy Land.`,
+    name:       "Israel",
+    tagline:    "Holy Land w/ Gold Leaf",
+    price:      180,
+    categories: ["Medium (5-7″)","With Gold Leaf","White Stone"],
+    description: `Soft pastels depict the land of Israel on white stone, with gold leaf lettering adding sacred warmth and intention. Fired in the kiln, each detail connects your doorway to the Holy Land and Hashem's holy mitzvah of putting  mezuzahs on each doorpost.`,
   },
 
   {
